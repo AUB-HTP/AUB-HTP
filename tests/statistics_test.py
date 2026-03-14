35ec90fbfd6a63a26a773492ad3f9cd60754b04c
Ahmad El Hajj <ahmadelhajj110@gmail.com>
2026-03-14 14:53:15 +0200

    more test ouline

--- tests/statistics_test.py
+++ tests/statistics_test.py
@@ -13,6 +13,21 @@ class TestAlphaPowerCorrectness:
 
     def test_alpha_power_alpha_1_matches_cauchy_scale_for_multivariate_data(self):
         ...
+    
+    @pytest.mark.parametrize(
+        "alpha", "scale", 
+        [
+            (0.2, -0.4),
+            (0.5, 2),
+            (1.0, 4),
+            (1.2, 10.),
+            (1.5, -20.),
+            (2., 40.),
+        ]    
+    )
+    def test_scale_equivariance_of_alpha_power(self, alpha: float, scale: float):
+        ...
+
 
 class TestAlphaLocationCorrectness:
     def test_alpha_location_alpha_2_matches_normal_location(self):
@@ -27,6 +42,7 @@ class TestAlphaLocationCorrectness:
     @pytest.mark.parametrize("alpha", [0.2, 0.3, 1.2, 1.5])
     def test_alpha_power_alpha_not_1_fails_for_multivariate_data(self, alpha: float):
         ...
+
 
 class TestDimensionsAndShapes:
     ...
